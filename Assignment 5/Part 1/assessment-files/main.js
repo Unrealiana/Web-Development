66872553c15776f7af973c35bda72ba1f0cb78c9
Unrealiana <156109303+Unrealiana@users.noreply.github.com>
2024-04-12 22:21:52 -0400

    initial commit

---
+++ Assignment 5/Part 1/assessment-files/main.js
@@ -0,0 +1,57 @@
+// functionality for showing/hiding the comments section 
+
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
+
+const transcriptBtn = document.querySelector('#transcript');
+const transcriptComment = document.querySelector('#transcriptComment');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+// transcript button functionality
+
+transcriptBtn.onclick = function(){
+if(transcriptComment.textContent === "") transcriptComment.textContent = "This isn't really an audio file about bears, but it is an audio file that you can transcribe.";
+else transcriptComment.textContent = "";
+}
+
+// functionality for adding a new comment via the comments form
+
+const form = document.querySelector('.comment-form');
+const nameField = document.querySelector('#name');
+const commentField = document.querySelector('#comment');
+const list = document.querySelector('.comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
+}
